fe9f39fca3c5c5d6b0ce25c6983a2955b4dea6f2
HoJin9622 <kiss0104040@gmail.com>
2020-06-05 01:06:17 +0900

    Defining Mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,39 @@
-export const people = [
+export let movies = [
   {
     id: 0,
-    name: "hojin",
-    age: 24,
-    gender: "male",
+    name: "Star Wars - The new one",
+    score: 1,
   },
   {
     id: 1,
-    name: "Daal",
-    age: 18,
-    gender: "male",
+    name: "Avengers - The new one",
+    score: 8,
   },
   {
     id: 2,
-    name: "JD",
-    age: 20,
-    gender: "female",
+    name: "The Godfather I",
+    score: 99,
   },
   {
     id: 3,
-    name: "flynn",
-    age: 19,
-    gender: "male",
+    name: "Logan",
+    score: 2,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 };
